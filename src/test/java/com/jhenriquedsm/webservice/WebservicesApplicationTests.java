1cfeb79fcd6a4d6e0503c873cdf7f43cf7bfb3b7
jhenriquedsm <jhsilvamata@gmail.com>
2025-01-06 13:30:34 -0300

    initial commit

---
+++ src/test/java/com/jhenriquedsm/webservice/WebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhenriquedsm.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
